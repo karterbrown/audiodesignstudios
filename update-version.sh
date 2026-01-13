f13c9b9b6ac781f0484faaab55aa1fd0254cdf00
Karter Brown <karterbrown@Koris-MacBook-Pro.local>
2026-01-13 11:21:25 -0700

    Fixed a bunch of stuff with the players. Made additional css pages for the html pages too.

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update Version Script
-# Run this script whenever you make changes to styles.css or content.js
+# Run this script whenever you make changes to CSS or JavaScript files
 # to ensure browsers reload the files instead of using cached versions
 
 # Generate new version number (current timestamp)
@@ -9,8 +9,13 @@ NEW_VERSION=$(date +%s)
 
 echo "Updating cache-busting version to: $NEW_VERSION"
 
-# Update all HTML files
-sed -i '' "s/styles\.css?v=[0-9]*/styles.css?v=$NEW_VERSION/g" *.html
+# Update all CSS file versions in HTML files
+sed -i '' "s/index\.css?v=[0-9]*/index.css?v=$NEW_VERSION/g" *.html
+sed -i '' "s/portfolio\.css?v=[0-9]*/portfolio.css?v=$NEW_VERSION/g" *.html
+sed -i '' "s/about\.css?v=[0-9]*/about.css?v=$NEW_VERSION/g" *.html
+sed -i '' "s/matrix\.css?v=[0-9]*/matrix.css?v=$NEW_VERSION/g" *.html
+
+# Update JavaScript file versions in HTML files
 sed -i '' "s/content\.js?v=[0-9]*/content.js?v=$NEW_VERSION/g" *.html
 
 echo "✓ Version updated in all HTML files"
