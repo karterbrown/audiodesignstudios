35e64f1d6388d39fde9b5df84a8bce53225619e0
Karter Brown <karterbrown@ad.weber.edu>
2026-03-24 12:35:34 -0600

    (Get-Content "C:\Users\karterbrown\Desktop\audiodesignstudios\picoanalyzer\index.html").Count

--- update-version.sh
+++ update-version.sh
@@ -12,11 +12,14 @@ echo "Updating cache-busting version to: $NEW_VERSION"
 # Update all CSS file versions in HTML files
 sed -i '' "s/index\.css?v=[0-9]*/index.css?v=$NEW_VERSION/g" *.html
 sed -i '' "s/editorial\.css?v=[0-9]*/editorial.css?v=$NEW_VERSION/g" *.html
-sed -i '' "s/compose\.css?v=[0-9]*/compose.css?v=$NEW_VERSION/g" *.html
-sed -i '' "s/matrix\.css?v=[0-9]*/matrix.css?v=$NEW_VERSION/g" *.html
+sed -i '' "s/compose\.css?v=[0-9]*/compose.css?v=$NEW_VERSION/g" compose/index.html
+sed -i '' "s/matrix\.css?v=[0-9]*/matrix.css?v=$NEW_VERSION/g" compose/index.html matrix/index.html
+sed -i '' "s/demo\.css?v=[0-9]*/demo.css?v=$NEW_VERSION/g" demo/index.html
+sed -i '' "s/processor\.css?v=[0-9]*/processor.css?v=$NEW_VERSION/g" processor/index.html
 
 # Update JavaScript file versions in HTML files
-sed -i '' "s/content\.js?v=[0-9]*/content.js?v=$NEW_VERSION/g" *.html
+sed -i '' "s/content\.js?v=[0-9]*/content.js?v=$NEW_VERSION/g" *.html compose/index.html
+sed -i '' "s/compose-players\.js?v=[0-9]*/compose-players.js?v=$NEW_VERSION/g" compose/index.html
 
 echo "✓ Version updated in all HTML files"
 echo "✓ New version: $NEW_VERSION"
