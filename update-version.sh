fc3172f5ccb2f37e14c1e19972e2be28b23986ad
karterbrown <music@kartergbrown.com>
2026-04-08 21:50:26 -0600

    Fix analyze button, add PowerShell deploy scripts, fix update-version.sh for Windows

--- update-version.sh
+++ update-version.sh
@@ -9,17 +9,27 @@ NEW_VERSION=$(date +%s)
 
 echo "Updating cache-busting version to: $NEW_VERSION"
 
+# Detect sed variant: GNU sed (Linux/Git-Bash) uses -i with no argument;
+# BSD sed (macOS) requires -i '' with an empty-string argument.
+if sed --version >/dev/null 2>&1; then
+  # GNU sed
+  SED() { sed -i "$@"; }
+else
+  # BSD sed (macOS)
+  SED() { sed -i '' "$@"; }
+fi
+
 # Update all CSS file versions in HTML files
-sed -i '' "s/index\.css?v=[0-9]*/index.css?v=$NEW_VERSION/g" *.html
-sed -i '' "s/editorial\.css?v=[0-9]*/editorial.css?v=$NEW_VERSION/g" *.html
-sed -i '' "s/compose\.css?v=[0-9]*/compose.css?v=$NEW_VERSION/g" compose/index.html
-sed -i '' "s/matrix\.css?v=[0-9]*/matrix.css?v=$NEW_VERSION/g" compose/index.html matrix/index.html
-sed -i '' "s/demo\.css?v=[0-9]*/demo.css?v=$NEW_VERSION/g" demo/index.html
-sed -i '' "s/processor\.css?v=[0-9]*/processor.css?v=$NEW_VERSION/g" processor/index.html
+SED "s/index\.css?v=[0-9]*/index.css?v=$NEW_VERSION/g" *.html
+SED "s/editorial\.css?v=[0-9]*/editorial.css?v=$NEW_VERSION/g" *.html
+SED "s/compose\.css?v=[0-9]*/compose.css?v=$NEW_VERSION/g" compose/index.html
+SED "s/matrix\.css?v=[0-9]*/matrix.css?v=$NEW_VERSION/g" compose/index.html matrix/index.html
+SED "s/demo\.css?v=[0-9]*/demo.css?v=$NEW_VERSION/g" demo/index.html
+SED "s/processor\.css?v=[0-9]*/processor.css?v=$NEW_VERSION/g" processor/index.html
 
 # Update JavaScript file versions in HTML files
-sed -i '' "s/content\.js?v=[0-9]*/content.js?v=$NEW_VERSION/g" *.html compose/index.html
-sed -i '' "s/compose-players\.js?v=[0-9]*/compose-players.js?v=$NEW_VERSION/g" compose/index.html
+SED "s/content\.js?v=[0-9]*/content.js?v=$NEW_VERSION/g" *.html compose/index.html
+SED "s/compose-players\.js?v=[0-9]*/compose-players.js?v=$NEW_VERSION/g" compose/index.html
 
 echo "✓ Version updated in all HTML files"
 echo "✓ New version: $NEW_VERSION"
